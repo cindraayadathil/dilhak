e8012fa831415de548f19f1ad2c1accd820f4063
jesnan <jsnnarayan@gmail.com>
2024-05-10 12:03:00 +0530

    banner

--- src/components/Banner1.js
+++ src/components/Banner1.js
@@ -1,13 +1,41 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import banner1 from '../images/Ban1.jpeg';
+import Ban5 from '../images/Ban5.png';
+import Ban2 from '../images/Ban2.avif';
+import Ban3 from '../images/Ban3.webp';
+import Ban4 from '../images/Ban4.png';
+import Ban6 from '../images/Ban6.avif';
+import Ban7 from '../images/Ban7.jpeg';
+import './Banner1.css'; // Import CSS file for styling
 
 function Banner1() {
   return (
-    <div className="Banner1">
-      {/* <Link to="/Bannerdetails"> */}
-        <img src={banner1} alt="Banner1" width='100%' height='800px' className="banner1-img" />
-      {/* </Link> */}
+    <div>
+     
+      <div className="scroll-container" >
+        {/* Image containers with images */}
+        <div className="image-container">
+        <img src={Ban5} alt="Ban5" width="350" height="800"/>
+        </div>
+        <div className="image-container">
+        <img src={Ban6} alt="Ban6" width="350" height="300"/>
+        </div>
+        <div className="image-container">
+        <img src={Ban6} alt="Ban6" width="350" height="300"/>
+        </div>
+        <div className="image-container">
+          <img src={Ban6} alt="Ban6" width="350" height="300"/>
+        </div>
+        <div className="image-container">
+          <img src={Ban6} alt="Ban6" width="350" height="300"/>
+        </div>
+        <div className="image-container">
+          <img src={Ban6} alt="Ban6" width="350" height="300"/>
+        </div>
+        <div className="image-container">
+        <img src={Ban6} alt="Ban6" width="350" height="300"/>
+        </div>
+      </div>
+      
     </div>
   );
 }
